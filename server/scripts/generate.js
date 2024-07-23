a0b84bfb21aebad325cf604eb1b4aa673ee10fbb
avirzin <arthur@virzin.me>
2024-07-23 18:03:38 -0300

    Added Ethereum address function

--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -1,5 +1,13 @@
 const { secp256k1 } = require("ethereum-cryptography/secp256k1");
 const { toHex } = require("ethereum-cryptography/utils");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+
+function getEthereumAddress(publicKey) {
+    const publicKeyHash = keccak256(publicKey.slice(1));
+    const ethereumAddress = publicKeyHash.slice(-20);
+    return toHex(ethereumAddress);
+}
+
 
 const privateKey = secp256k1.utils.randomPrivateKey();
 
@@ -7,4 +15,6 @@ console.log('private key:', toHex(privateKey));
 
 const publicKey = secp256k1.getPublicKey(privateKey);
 
-console.log('public key:', toHex(publicKey));
+console.log('public key (full):', toHex(publicKey));
+
+console.log('public key (etherem):', getEthereumAddress(publicKey));
