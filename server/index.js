6f8c9b7a30b5650d3358f86bde5a0c4ac8c6182f
avirzin <arthur@virzin.me>
2024-07-23 22:41:08 -0300

    Now it is working

--- server/index.js
+++ server/index.js
@@ -7,9 +7,9 @@ app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  "02e619b0b027a1bd852405c0046754797d9e270f9653a3238ee51f2b643b704624": 100,
+  "0212cb92d3efd646180462fd0cd8506dad997cdce4ca156eb2a27b3b76329a8af3": 50,
+  "02f0cc384f75805e8925d58d26093ebabf56b5217f768da1e077e878a225fb409b": 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,17 +19,37 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, amount, signature, recovery} = req.body;
 
-  setInitialBalance(sender);
-  setInitialBalance(recipient);
+  if(!signature) res.status(404).send({ message: "Signature is missing" });
+  if(!recovery) res.status(400).send({ message: "Recovery is missing" });
 
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+  try {
+    
+    const bytes = utf8ToBytes(JSON.stringify({ sender, recipient, amount }));
+    const hash = keccak256(bytes);
+
+    const sig = new Uint8Array(signature);
+
+    const publicKey = await secp.recoverPublicKey(hash, sig, recovery);
+
+    if(toHex(publicKey) !== sender){
+      res.status(400).send({ message: "signature no is valid" });
+    }
+
+
+    setInitialBalance(sender);
+    setInitialBalance(recipient);
+
+    if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+    }
+  } catch (error) {
+    console.log
   }
 });
 
--- server/scripts/generate.js
+++ server/scripts/generate.js
@@ -2,19 +2,20 @@ const { secp256k1 } = require("ethereum-cryptography/secp256k1");
 const { toHex } = require("ethereum-cryptography/utils");
 const { keccak256 } = require("ethereum-cryptography/keccak");
 
+// Getting ethereum address from public key
 function getEthereumAddress(publicKey) {
     const publicKeyHash = keccak256(publicKey.slice(1));
     const ethereumAddress = publicKeyHash.slice(-20);
     return toHex(ethereumAddress);
 }
 
-
+// Private key generation
 const privateKey = secp256k1.utils.randomPrivateKey();
-
 console.log('private key:', toHex(privateKey));
 
+// Public key generation
 const publicKey = secp256k1.getPublicKey(privateKey);
-
 console.log('public key (full):', toHex(publicKey));
 
-console.log('public key (etherem):', getEthereumAddress(publicKey));
+// Ethereum address generation
+console.log('public key (ethereum):', getEthereumAddress(publicKey));
